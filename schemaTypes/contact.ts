fe76961f8e18344f9419b84a71fae8aa89dc9296
Hussnain Ahmed <143833798+hussnain-143@users.noreply.github.com>
2025-09-01 18:05:17 +0500

    set the advertise and contact schema

--- schemaTypes/contact.ts
+++ schemaTypes/contact.ts
@@ -1,4 +1,3 @@
-
 export default {
   name: 'contactFormSubmission',
   title: 'Contact Form Submission',
@@ -8,26 +7,31 @@ export default {
       name: 'name',
       title: 'Name',
       type: 'string',
+      readOnly: true,
     },
     {
       name: 'email',
       title: 'Email',
       type: 'string',
+      readOnly: true,
     },
     {
       name: 'question',
       title: 'Question',
       type: 'string',
+      readOnly: true,
     },
     {
       name: 'message',
       title: 'Message',
       type: 'text',
+      readOnly: true,
     },
     {
       name: 'createdAt',
       title: 'Created At',
       type: 'datetime',
+      readOnly: true,
       initialValue: () => new Date().toISOString(),
     },
   ],
